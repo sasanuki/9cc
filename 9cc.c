a44e3ae90200084188ba9bcf4c4c49eeeed5bde8
sasanuki <sasanuki.tokyo@gmail.com>
2020-02-12 19:59:56 +0900

    add error position

--- 9cc.c
+++ 9cc.c
@@ -22,9 +22,25 @@ struct Token {
 
 // 現在着目しているトークン
 Token *token;
+// 入力プログラム
+char *user_input;
 
 // エラー報告関数
 // printfと同じ引数
+void error_at(char *loc, char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "*s", pos, "");
+    fprintf(stderr, "^ ");
+
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 void error(char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
@@ -45,14 +61,14 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告
 void expect(char op){
     if(token->kind != TK_RESERVED || token->str[0] != op){
-        error("'%C'ではありません", op);
+        error_at(token->str, "'%C'ではありません", op);
     }
     token = token->next;
 }
 
 int expect_number(){
     if(token->kind != TK_NUM){
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -107,6 +123,7 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    user_input = argv[1];
     token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
